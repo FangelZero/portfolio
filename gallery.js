726baa2c422e3eba6b530346c9064329afa7d50c
Cane1712 <cane17124@gmail.com>
2025-07-05 21:18:58 +0200

    Fixed popup

--- gallery.js
+++ gallery.js
@@ -1,20 +1,38 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("modal");
+const modalImage = modal.querySelector("img")
+const caption = document.getElementById("caption");
+const span = document.getElementById("close");
+const allImages = document.querySelectorAll(".row .column img")
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+allImages.forEach(img => {
+  if (!img || img == undefined || img == null || !img instanceof HTMLImageElement) return;
+  if (img instanceof HTMLImageElement) {
+    img.onclick = function () {
+      modal.style.display = "block";
+      modalImage.src = img.src
+      caption.textContent = img.alt
+    }
+  }
+})
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+span.onclick = () => modal.style.display = "none";
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById("myImg");
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
